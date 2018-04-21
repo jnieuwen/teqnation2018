44039a2986969e0983ed8d4599b93b53aa175c46
Jeroen van Nieuwenhuizen <jeroen@jeroen.se>
2018-04-21 10:42:13 +0200

    Created prod2 branch

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html>
   <head>
     <title>DrySalt TEST Website - not for production</title>
+    <link href="slick.css" rel="stylesheet">
+    <link href="slick-theme.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <script src="jquery.min.js"></script>
   </head>
   <body>
     <section>
@@ -15,6 +18,31 @@
     </section>
 
 
+
+    <section>
+      <div class="slider">
+        <div>
+          <!-- picture from Deliciously Yum! -->
+          <img src="SaltedChewyCaramels1.jpg" width="300" height="150">
+        </div>
+        <div>
+          <!-- picture from Food Network! -->
+          <img src="1459798109068.jpg" width="300" height="150">
+        </div>
+        <div>
+          <!-- picture from Salt Revolution! -->
+          <img src="Sea-Salt-Caramels-300x240.jpg" width="300" height="150">
+        </div>
+        <div>
+          <!-- picture from SugarHero! -->
+          <img src="salted-caramel-bars-1.jpg" width="300" height="150">
+        </div>
+      </div>
+    </section>
+
+    <!-- picture from pixabay -->
+    <div class="parallax-container" data-parallax="scroll" data-bleed="10" data-speed="0.2" data-image-src="salt-lake-594606_1920.jpg" data-natural-width="1920" data-natural-height="1321"></div>
+
     <section>
       <div class="container">
         <p>We at DrySalt value caramel and provide awesome pictures of it ... that's what we do. Naturally powered by SaltStack!
@@ -22,66 +50,39 @@
       </div>
     </section>
 
-    <div class="boxed">
-      <div class="bss-slides slides">
-        <!-- pictures from https://www.cookingclassy.com/salted-caramel-sauce-step-step-pictures/ -->
-        <figure>
-          <img src="salted-caramel-sauce-10.jpg" width="100%" />
-          <figcaption>Add ingredients</figcaption>
-        </figure>
-        <figure>
-          <img src="salted-caramel-sauce-11.jpg" width="100%" />
-          <figcaption>Melt the sugar</figcaption>
-        </figure>
-        <figure>
-          <img src="salted-caramel-sauce-12.jpg" width="100%" />
-          <figcaption>Watch the temperature, color is good, burning isn't</figcaption>
-        </figure>
-        <figure>
-          <img src="salted-caramel-sauce-25.jpg" width="100%" />
-          <figcaption>At this nice coloing, lower temperature and add butter</figcaption>
-        </figure>
-        <figure>
-          <img src="salted-caramel-sauce-23.jpg" width="100%" />
-          <figcaption>Add butter, careful it will rise up (lower temperature when it does)</figcaption>
-        </figure>
-        <figure>
-          <img src="salted-caramel-sauce-22.jpg" width="100%" />
-          <figcaption>Add double cream</figcaption>
-        </figure>
-        <figure>
-          <img src="salted-caramel-sauce-21.jpg" width="100%" />
-          <figcaption>Add shaved salt-flakes and stir</figcaption>
-        </figure>
-        <figure>
-          <img src="salted-caramel-sauce-9-768x1152.jpg" width="100%" />
-          <figcaption>Enjoy!</figcaption>
-        </figure>
-      </div>
-    </div>
-
-
 
     <footer>
       <div class="container">
 	Copyright &copy; 2018 by WebSalt Inc. - the imaginary company for our <a href="https://sue2018.snow.nl">SUE 2018</a> workshop 
       </div>
     </footer>
-    <script src="better-simple-slideshow.js"></script>
+    <script src="slick.min.js"></script>
     <script type="text/javascript">
-      var opts = {
-        auto : { 
-                // speed to advance slides at. accepts number of milliseconds
-                speed : 2500, 
-                // pause advancing on mouseover? accepts boolean
-                pauseOnHover : false 
-            },
-            // show fullscreen toggle? accepts boolean
-            fullScreen : true, 
-            // support swiping on touch devices? accepts boolean, requires hammer.js
-            swipe : false 
-        };
-      makeBSS('.slides',opts);
+      $(document).ready(function(){
+        $('.slider').slick({
+           infinite: true ,
+           autoplay: true,
+           slidesToShow: 3,
+           autoplaySpeed: 2000,
+           // the magic
+           responsive: [{
+               breakpoint: 1024,
+               settings: {
+                 slidesToShow: 3,
+                 infinite: true
+               }
+             }, {
+               breakpoint: 600,
+                 settings: {
+                 slidesToShow: 2,
+                 dots: true
+               }
+             }, {
+               breakpoint: 300,
+               settings: "unslick" // destroys slick
+             }]
+        });
+      });
     </script>
   </body>
 </html>
